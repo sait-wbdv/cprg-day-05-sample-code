75dbf4cf4444ff3c4b5e26f477bbaeddce3f520e
Ashlyn Knox <ashlyn@lilyx.io>
2024-01-19 11:15:07 -0700

    template setup

---
+++ components/AppHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <div>LOGO</div>
+    <nav>
+      <NuxtLink to="login">Log In</NuxtLink>
+      <NuxtLink to="signup">Sign Up</NuxtLink>
+    </nav>
+    <div>
+      <Logout />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: black;
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+}
+nav {
+  display: flex;
+  justify-content: space-around;
+  gap: 2rem;
+}
+a,
+div {
+  color: white;
+  text-decoration: none;
+}
+a:hover {
+  color: lightblue;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <h1>Login Page</h1>
+  </main>
+</template>
